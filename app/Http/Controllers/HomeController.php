891c114bf58b9a6aa94976fdca627cc2b912015b
Vitor de Sousa <vitordessilva@gmail.com>
2026-05-04 18:25:23 -0300

    feat: Controller to guest user list contacts

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\ContactRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    public function index(Request $request, ContactRepository $repository): View
+    {
+        try {
+
+            $contacts = $repository->listAndFilter(
+                page: $request->query('page', 1),
+                perPage: $request->query('per_page', 10),
+            );
+
+            $header = __('Contacts');
+
+            return view('dashboard.contacts.index', compact('contacts', 'header'));
+
+        } catch (\Throwable $throwable) {
+            Log::error('Error fetching contacts: ' . $throwable->getMessage(), [
+                'stack_trace' => $throwable->getTraceAsString(),
+            ]);
+
+            abort(Response::HTTP_INTERNAL_SERVER_ERROR, __('An error occurred while fetching contacts.'));
+        }
+    }
+}
