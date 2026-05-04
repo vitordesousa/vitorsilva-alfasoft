15fa36319bccba863f57ae189bd3abf87ebc1349
Vitor de Sousa <vitordessilva@gmail.com>
2026-05-04 18:23:25 -0300

    feat: app routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +15,33 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', [HomeController::class, 'index'])
+    //->middleware('throttle:10,1')
+    ->name('home.index');
+
+Route::middleware('auth')
+    ->prefix('dashboard/contacts')
+    ->name('dashboard.contacts.')
+    ->group(function () {
+    Route::get('/', [ContactController::class, 'index'])->name('index');
+    Route::get('/create', [ContactController::class, 'create'])->name('create');
+    Route::post('/', [ContactController::class, 'store'])->name('store');
+    Route::get('/{contact}', [ContactController::class, 'show'])->name('show');
+    Route::get('/{contact}/edit', [ContactController::class, 'edit'])->name('edit');
+    Route::put('/{contact}', [ContactController::class, 'update'])->name('update');
+    Route::delete('/{contact}', [ContactController::class, 'destroy'])->name('destroy');
 });
+
+
+
+/**
+ * Default Laravel Breeze routes for authentication and dashboard access.
+ */
 
 Route::get('/dashboard', function () {
     return view('dashboard');
